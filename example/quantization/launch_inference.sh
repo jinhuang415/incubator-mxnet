d6731ad64313c5c204ee4413ade3025b2c79dafa
xinyu-intel <xinyu1.chen@intel.com>
2018-06-14 22:15:06 +0800

    add vgg models

--- example/quantization/launch_inference.sh
+++ example/quantization/launch_inference.sh
@@ -43,3 +43,16 @@ python imagenet_inference.py --symbol-file=./model/imagenet1k-inception-bn-quant
 python imagenet_inference.py --symbol-file=./model/imagenet1k-inception-bn-quantized-5batches-entropy-symbol.json --param-file=./model/imagenet1k-inception-bn-quantized-0000.params --rgb-mean=123.68,116.779,103.939 --num-skipped-batches=50 --num-inference-batches=500 --dataset=./data/val_256_q90.rec
 python imagenet_inference.py --symbol-file=./model/imagenet1k-inception-bn-quantized-10batches-entropy-symbol.json --param-file=./model/imagenet1k-inception-bn-quantized-0000.params --rgb-mean=123.68,116.779,103.939 --num-skipped-batches=50 --num-inference-batches=500 --dataset=./data/val_256_q90.rec
 python imagenet_inference.py --symbol-file=./model/imagenet1k-inception-bn-quantized-50batches-entropy-symbol.json --param-file=./model/imagenet1k-inception-bn-quantized-0000.params --rgb-mean=123.68,116.779,103.939 --num-skipped-batches=50 --num-inference-batches=500 --dataset=./data/val_256_q90.rec
+
+python imagenet_inference.py --symbol-file=./model/imagenet1k-vgg-16-symbol.json --param-file=./model/imagenet1k-vgg-16-0000.params --label-name='prob_label' --rgb-mean=0,0,0 --num-skipped-batches=50 --num-inference-batches=500 --dataset=./data/val_256_q90.rec
+
+python imagenet_inference.py --symbol-file=./model/imagenet1k-vgg-16-quantized-symbol.json --param-file=./model/imagenet1k-vgg-16-quantized-0000.params --label-name='prob_label' --rgb-mean=0,0,0 --num-skipped-batches=50 --num-inference-batches=500 --dataset=./data/val_256_q90.rec
+
+python imagenet_inference.py --symbol-file=./model/imagenet1k-vgg-16-quantized-5batches-naive-symbol.json --param-file=./model/imagenet1k-vgg-16-quantized-0000.params --label-name='prob_label' --rgb-mean=0,0,0 --num-skipped-batches=50 --num-inference-batches=500 --dataset=./data/val_256_q90.rec
+python imagenet_inference.py --symbol-file=./model/imagenet1k-vgg-16-quantized-10batches-naive-symbol.json --param-file=./model/imagenet1k-vgg-16-quantized-0000.params --label-name='prob_label' --rgb-mean=0,0,0 --num-skipped-batches=50 --num-inference-batches=500 --dataset=./data/val_256_q90.rec
+python imagenet_inference.py --symbol-file=./model/imagenet1k-vgg-16-quantized-50batches-naive-symbol.json --param-file=./model/imagenet1k-vgg-16-quantized-0000.params --label-name='prob_label' --rgb-mean=0,0,0 --num-skipped-batches=50 --num-inference-batches=500 --dataset=./data/val_256_q90.rec
+
+python imagenet_inference.py --symbol-file=./model/imagenet1k-vgg-16-quantized-5batches-entropy-symbol.json --param-file=./model/imagenet1k-vgg-16-quantized-0000.params --label-name='prob_label' --rgb-mean=0,0,0 --num-skipped-batches=50 --num-inference-batches=500 --dataset=./data/val_256_q90.rec
+python imagenet_inference.py --symbol-file=./model/imagenet1k-vgg-16-quantized-10batches-entropy-symbol.json --param-file=./model/imagenet1k-vgg-16-quantized-0000.params --label-name='prob_label' --rgb-mean=0,0,0 --num-skipped-batches=50 --num-inference-batches=500 --dataset=./data/val_256_q90.rec
+python imagenet_inference.py --symbol-file=./model/imagenet1k-vgg-16-quantized-50batches-entropy-symbol.json --param-file=./model/imagenet1k-vgg-16-quantized-0000.params --label-name='prob_label' --rgb-mean=0,0,0 --num-skipped-batches=50 --num-inference-batches=500 --dataset=./data/val_256_q90.rec
+
